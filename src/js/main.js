8ab66f08437c56e2e1c28b9da87588df306b03f3
Nadezhda Glazacheva <glaznad75@gmail.com>
2023-02-28 00:06:30 +0700

    first comment

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 burgerHandler();
 dropdownHandler();
+searchHandler();
 
 function burgerHandler() {
   const header = document.querySelector(".header");
-  const iconMenu = document.querySelector(".icon-menu");
-  const menuBody = document.querySelector(".menu__body");
+  const iconMenu = header.querySelector(".icon-menu");
+  const menuBody = header.querySelector(".menu__body");
 
-  iconMenu?.addEventListener("click", function (e) {
+  iconMenu?.addEventListener("click", function () {
     iconMenu.classList.toggle("_active");
     menuBody.classList.toggle("_active");
   });
@@ -16,9 +17,22 @@ function dropdownHandler() {
   const footerMenuItems = document.querySelectorAll(".footer-menu li");
 
   for (const item of footerMenuItems) {
-    item.addEventListener("click", () => {
-      item.querySelector(".footer-menu__item")?.classList.toggle("_active");
-      item.querySelector(".footer-menu__submenu")?.classList.toggle("_active");
+    item.addEventListener("click", function () {
+      this.querySelector(".footer-menu__item")?.classList.toggle("_active");
+      this.querySelector(".footer-menu__submenu")?.classList.toggle("_active");
     });
   }
+}
+
+function searchHandler() {
+  const searchBtn = document.querySelector(".header__search-btn");
+
+  searchBtn?.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(".header__search-form")?.classList.toggle("_active");
+    document
+      .querySelector(".header__search-input")
+      ?.classList.toggle("_active");
+    this.classList.toggle("_active");
+  });
 }
